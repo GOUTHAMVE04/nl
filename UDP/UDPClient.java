86f7f6c7fa73db1099f8ddeddd51d19de5e16aa9
GOUTHAMVE04 <vegoutham@gmail.com>
2026-03-27 22:10:16 +0530

    udp new

--- UDP/UDPClient.java
+++ UDP/UDPClient.java
@@ -3,40 +3,31 @@ import java.net.*;
 
 public class UDPClient {
     public static void main(String[] args) throws Exception {
-        DatagramSocket client = new DatagramSocket(); // [cite: 755]
-        InetAddress serverIP = InetAddress.getByName("localhost"); // [cite: 756]
-        int serverPort = 5555;
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in)); // [cite: 757]
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress serverIp = InetAddress.getByName("localhost");
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        byte[] buf = new byte[1024];
 
         while (true) {
             // 1. Client user types a message to send to Server
             System.out.print("Enter message for Server: ");
-            String clientMsg = console.readLine(); // [cite: 786]
-            byte[] sendData = clientMsg.getBytes(); // [cite: 787]
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverIP, serverPort); // [cite: 788]
-            client.send(sendPacket); // [cite: 789]
+            String msg = console.readLine();
+            byte[] data = msg.getBytes();
+            socket.send(new DatagramPacket(data, data.length, serverIp, 5555));
 
             // 2. Wait for Server's Acknowledgement and print it
-            byte[] recBuffer1 = new byte[1024];
-            DatagramPacket receivePacket1 = new DatagramPacket(recBuffer1, recBuffer1.length); // [cite: 772]
-            client.receive(receivePacket1); // [cite: 773]
-            
-            String serverAck = new String(receivePacket1.getData(), 0, receivePacket1.getLength()); // [cite: 774]
-            System.out.println(serverAck);
+            DatagramPacket ackPkt = new DatagramPacket(buf, buf.length);
+            socket.receive(ackPkt);
+            System.out.println("System: " + new String(ackPkt.getData(), 0, ackPkt.getLength()));
 
             // 3. Wait for Server's new message and print it
-            byte[] recBuffer2 = new byte[1024];
-            DatagramPacket receivePacket2 = new DatagramPacket(recBuffer2, recBuffer2.length);
-            client.receive(receivePacket2);
-            
-            String serverMsg = new String(receivePacket2.getData(), 0, receivePacket2.getLength());
-            System.out.println("From Server: " + serverMsg);
+            DatagramPacket msgPkt = new DatagramPacket(buf, buf.length);
+            socket.receive(msgPkt);
+            System.out.println("From Server: " + new String(msgPkt.getData(), 0, msgPkt.getLength()));
 
             // 4. Send automatic Acknowledgement to Server
-            String ackMsg = "Client ACK: I received your message!";
-            byte[] ackData = ackMsg.getBytes();
-            DatagramPacket ackPacket = new DatagramPacket(ackData, ackData.length, serverIP, serverPort);
-            client.send(ackPacket);
+            byte[] ackMsg = "Client ACK: I received your message!".getBytes();
+            socket.send(new DatagramPacket(ackMsg, ackMsg.length, serverIp, 5555));
             System.out.println("--------------------------------");
         }
     }
--- UDP/UDPServer.java
+++ UDP/UDPServer.java
@@ -3,43 +3,34 @@ import java.net.*;
 
 public class UDPServer {
     public static void main(String[] args) throws Exception {
-        DatagramSocket server = new DatagramSocket(5555); // [cite: 684]
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in)); // [cite: 685]
-        System.out.println("UDP Server waiting for packets...");
+        DatagramSocket socket = new DatagramSocket(5555);
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        byte[] buf = new byte[1024];
+        System.out.println("Server waiting for packets...");
 
         while (true) {
             // 1. Wait for Client's message and print it
-            byte[] recBuffer1 = new byte[1024];
-            DatagramPacket receivePacket1 = new DatagramPacket(recBuffer1, recBuffer1.length); // [cite: 702]
-            server.receive(receivePacket1); // [cite: 703]
-            
-            String clientMsg = new String(receivePacket1.getData(), 0, receivePacket1.getLength()); // [cite: 706]
+            DatagramPacket clientPkt = new DatagramPacket(buf, buf.length);
+            socket.receive(clientPkt);
+            String clientMsg = new String(clientPkt.getData(), 0, clientPkt.getLength());
             System.out.println("From Client: " + clientMsg);
 
-            // CRITICAL: Save the Client's IP and Port from their packet so we can reply!
-            InetAddress clientIP = receivePacket1.getAddress(); // [cite: 704]
-            int clientPort = receivePacket1.getPort(); // [cite: 705]
-
             // 2. Send automatic Acknowledgement to Client
-            String ackMsg = "Server ACK: I received your message!";
-            byte[] ackData = ackMsg.getBytes();
-            DatagramPacket ackPacket = new DatagramPacket(ackData, ackData.length, clientIP, clientPort);
-            server.send(ackPacket); // [cite: 720]
+            InetAddress clientIp = clientPkt.getAddress();
+            int clientPort = clientPkt.getPort();
+            byte[] ackData = "Server ACK: I received your message!".getBytes();
+            socket.send(new DatagramPacket(ackData, ackData.length, clientIp, clientPort));
 
             // 3. Server user types a message to send to Client
             System.out.print("Enter message for Client: ");
-            String serverMsg = console.readLine(); // [cite: 716]
-            byte[] sendData = serverMsg.getBytes(); // [cite: 717]
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientIP, clientPort); // [cite: 718, 719]
-            server.send(sendPacket);
+            String serverMsg = console.readLine();
+            byte[] serverData = serverMsg.getBytes();
+            socket.send(new DatagramPacket(serverData, serverData.length, clientIp, clientPort));
 
             // 4. Wait for Client's Acknowledgement and print it
-            byte[] recBuffer2 = new byte[1024];
-            DatagramPacket receivePacket2 = new DatagramPacket(recBuffer2, recBuffer2.length);
-            server.receive(receivePacket2);
-            
-            String clientAck = new String(receivePacket2.getData(), 0, receivePacket2.getLength());
-            System.out.println(clientAck);
+            DatagramPacket clientAck = new DatagramPacket(buf, buf.length);
+            socket.receive(clientAck);
+            System.out.println("System: " + new String(clientAck.getData(), 0, clientAck.getLength()));
             System.out.println("--------------------------------");
         }
     }
